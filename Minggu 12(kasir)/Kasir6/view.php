d29c5307b41fb69d67081f6df6eef11584126ab5
bafanzul <bayu_23520004@stimata.ac.id>
2024-06-27 22:46:02 +0700

    update

--- Minggu 12(kasir)/Kasir6/view.php
+++ Minggu 12(kasir)/Kasir6/view.php
@@ -132,7 +132,7 @@ if(isset($_GET['idp'])){
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Rangga 2024</div>
+                            <div class="text-muted">Copyright &copy; Bafanzul 2024</div>
                         </div>
                     </div>
                 </footer>
