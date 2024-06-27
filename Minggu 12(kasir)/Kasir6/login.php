7913a75c50bd5a27820e285ab55a7cc49dcb4cbe
bafanzul <bayu_23520004@stimata.ac.id>
2024-06-27 23:25:24 +0700

    update

--- Minggu 12(kasir)/Kasir6/login.php
+++ Minggu 12(kasir)/Kasir6/login.php
@@ -30,7 +30,7 @@ if(!isset($_SESSION['login'])){
                         <div class="row justify-content-center">
                             <div class="col-lg-5">
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Masuk Aplikasi</h3></div>
                                     <div class="card-body">
                                         <form method="POST">
                                             <div class="form-floating mb-3">
@@ -42,7 +42,7 @@ if(!isset($_SESSION['login'])){
                                                 <label for="inputPassword">Password</label>
                                             </div>
                                             <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                                             <button type="submit" name="login" class="btn btn-primary">Login
+                                             <button type="submit" name="login" class="btn btn-primary">Masuk
                                              </button>
                                                 
                                             </div>
